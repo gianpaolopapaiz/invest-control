fa2707bd27e23cd1ce7a7952729952de005b4863
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2021-07-20 19:36:48 -0300

    added value_year to cdi validation

--- app/models/cdi.rb
+++ app/models/cdi.rb
@@ -1,4 +1,4 @@
 class Cdi < ApplicationRecord
-  validates :value_month, :value_day, :date_update, presence: :true
+  validates :value_month, :value_day, :value_year, :date_update, presence: :true
   validates :date_tax, uniqueness: true
 end
