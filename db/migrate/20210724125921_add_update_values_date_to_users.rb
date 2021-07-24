afc4d91311818d27ac5493faa1e7003fe3cef761
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2021-07-24 10:45:48 -0300

    removed migrations to fix bug

--- db/migrate/20210724125921_add_update_values_date_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddUpdateValuesDateToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :update_values_date, :date, default: Date.new(20210, 1, 1)
-  end
-end
--- db/migrate/20210724131113_remove_update_values_date_to_users_again.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUpdateValuesDateToUsersAgain < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :users, :update_values_date
-  end
-end
